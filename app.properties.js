224e23a586ebdb952db465eb2791eb6c61c529d4
s3698211@rmit.edu.vn <s3698211@rmit.edu.vn>
2021-11-24 21:47:18 +1100

    fixing config

--- app.properties.js
+++ app.properties.js
@@ -1,5 +1,3 @@
-exports.USERNAME = "b021e8d008e51e";
-exports.PASSWORD = "5f435de7";
-exports.HOSTNAME = "us-cdbr-east-04.cleardb.com";
-exports.INITIAL_SCHEMA = "heroku_5fb2135de2b5866";
+exports.USERNAME = "tohoangkhoiaus@gmail.com";
+exports.PASSWORD = "Kingsbury22";
 exports.SECRET = "KEY";
