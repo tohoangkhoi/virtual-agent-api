1e68749ae4b6244fcca79bb0321e8035921f5441
tohoangkhoi <95118667+tohoangkhoi@users.noreply.github.com>
2022-07-18 15:18:56 +1000

    Update app.properties.js

--- app.properties.js
+++ app.properties.js
@@ -1,5 +1,5 @@
-exports.USERNAME = "tohoangkhoiaus@gmail.com";
-exports.PASSWORD = "Kingsbury22";
+exports.USERNAME = "YOUR_DB_USERNAME";
+exports.PASSWORD = "YOUR_PASSWORD";
 exports.SECRET = "KEY";
 exports.CHAT_ENGINE_PROJECT_ID = "6f3cff72-4dd3-4f2f-bfdc-499d800bd7e3";
 exports.CHAT_ENGINE_PROJECT_SECRET = "b34ba067-56e7-4a01-b9a3-d9bf32975370";
